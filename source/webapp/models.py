1849b038249068535385213eaf62b839cf52c6c7
Emil <emilrakaev@gmail.com>
2020-10-24 11:21:33 +0600

    Созданна модель фотографии

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    photo = models.ImageField(upload_to='user_pics', verbose_name='Картинка')
+    sign = models.CharField(max_length=100, verbose_name='Подпись')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='photos',
+                               verbose_name='Автор')
+
+    def __str__(self):
+        return self.sign
+
+    class Meta:
+        verbose_name = 'Фотография'
+        verbose_name_plural = 'Фотографии'
+
+
+# class Favorites(models.Model):
+#
